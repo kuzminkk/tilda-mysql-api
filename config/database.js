d735bbfa1acf572ac419181078f83592779562e9
kuzminkk <kuzminkk5@gmail.com>
2025-12-16 17:33:49 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,21 @@
+// --------------------------------------------
+// КОНФИГУРАЦИЯ ПОДКЛЮЧЕНИЯ К БАЗЕ ДАННЫХ
+// --------------------------------------------
 
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+  host: process.env.DB_HOST,      // Хост БД
+  user: process.env.DB_USER,      // Имя пользователя БД
+  password: process.env.DB_PASS,  // Пароль пользователя БД
+  database: process.env.DB_NAME,  // Название базы данных
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306, // Порт (по умолчанию 3306)
+};
+
+// Экспортируем конфигурацию
+export { dbConfig };
+
+// Функция для создания подключения
+export const createConnection = async () => {
+  return await mysql.createConnection(dbConfig);
+};
