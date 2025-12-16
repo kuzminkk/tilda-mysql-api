072b1ff40cb13f34916107241a57ddc7ec790260
kuzminkk <kuzminkk5@gmail.com>
2025-12-16 17:34:01 +0300

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,1 +1,29 @@
+// --------------------------------------------
+// НАСТРОЙКА CORS (БЕЗОПАСНОСТЬ)
+// --------------------------------------------
 
+// Список разрешенных доменов для CORS (доступ к API)
+const allowedOrigins = [
+  "https://project16054216.tilda.ws",
+  "http://project16054216.tilda.ws",
+  "http://systemdental.tilda.ws",
+  "https://project17567096.tilda.ws",
+  "http://project17567096.tilda.ws", 
+  "http://systemdental.tilda.ws",
+  "https://systemdental.tilda.ws",
+  "https://tilda.ws"
+];
+
+// Настройка CORS
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Разрешаем запросы из разрешенных источников
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+export { allowedOrigins, corsOptions };
